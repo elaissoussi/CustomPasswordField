3a5638f4100fb017ce2ddf45658d254ae7b0e977
elaissoussi <monsif20@gmail.com>
2014-03-21 15:29:22 +0000

    CustomPasswordField

---
+++ widget.js
@@ -0,0 +1,14 @@
+WAF.define('CustomPasswordField', ['waf-core/widget'], function(widget) {
+
+    var CustomPasswordField = widget.create('CustomPasswordField', 'CustomTextField', {
+        init: function() {
+            this.node.setAttribute("type", "password");
+            this.node.placeholder ="Tape a Password";
+        }
+    });
+
+    return CustomPasswordField;
+
+});
+
+/* For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3871.html */
